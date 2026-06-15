55fbd537db33d1afa50af04c2273447584fd4cff
Devops Suren <rajsuren1210@gmail.com>
2026-06-15 21:36:30 +0530

    fmt the tf code

--- Terrafrom/main.tf
+++ Terrafrom/main.tf
@@ -42,23 +42,23 @@ locals {
 # VPC Module
 ##########################################
 module "vpc" {
-  source = "./Module/VPC"
-  vpc_name = var.project_name
-  vpc_cidr = var.vpc_cidr
-  public_subnet_cidrs = var.public_subnet_cidrs
+  source               = "./Module/VPC"
+  vpc_name             = var.project_name
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
-  azs = var.azs
-  tags = local.common_tags
+  azs                  = var.azs
+  tags                 = local.common_tags
 }
 
 ##########################################
 # Security Group Module
 ##########################################
 module "security_group" {
-  source = "./Module/SG"
+  source   = "./Module/SG"
   vpc_name = var.project_name
-  vpc_id = module.vpc.vpc_id
-  tags = local.common_tags
+  vpc_id   = module.vpc.vpc_id
+  tags     = local.common_tags
 
 }
 ##########################################
@@ -66,20 +66,20 @@ module "security_group" {
 ##########################################
 
 module "alb" {
-  source = "./Module/ALB"
-  vpc_name = var.project_name
-  vpc_id = module.vpc.vpc_id
+  source            = "./Module/ALB"
+  vpc_name          = var.project_name
+  vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.vpc.public_subnet_ids
-  alb_sg_id = module.security_group.alb_sg_id
-  tags = local.common_tags
+  alb_sg_id         = module.security_group.alb_sg_id
+  tags              = local.common_tags
 }
 ##########################################
 # ECR Module
 ##########################################
 module "ecr" {
-  source = "./Module/ECR"
+  source          = "./Module/ECR"
   repository_name = "${var.project_name}-app"
-  tags = local.common_tags
+  tags            = local.common_tags
 
 }
 ##########################################
@@ -87,28 +87,28 @@ module "ecr" {
 ##########################################
 
 module "ecs" {
-  source = "./Module/ECS"
-  vpc_name = var.project_name
+  source             = "./Module/ECS"
+  vpc_name           = var.project_name
   private_subnet_ids = module.vpc.private_subnet_ids
-  ecs_sg_id = module.security_group.ecs_sg_id
-  target_group_arn = module.alb.target_group_arn
-  repository_url = module.ecr.repository_url
-  aws_region = var.aws_region
-  tags = local.common_tags
+  ecs_sg_id          = module.security_group.ecs_sg_id
+  target_group_arn   = module.alb.target_group_arn
+  repository_url     = module.ecr.repository_url
+  aws_region         = var.aws_region
+  tags               = local.common_tags
 }
 ##########################################
 # RDS Module
 ##########################################
 
 module "rds" {
-  source = "./Module/RDS"
-  vpc_name = var.project_name
+  source             = "./Module/RDS"
+  vpc_name           = var.project_name
   private_subnet_ids = module.vpc.private_subnet_ids
-  rds_sg_id = module.security_group.rds_sg_id
-  db_name = var.db_name
-  username = var.db_username
-  password = var.db_password
-  tags = local.common_tags
+  rds_sg_id          = module.security_group.rds_sg_id
+  db_name            = var.db_name
+  username           = var.db_username
+  password           = var.db_password
+  tags               = local.common_tags
 
 }
 #########################################
@@ -116,11 +116,11 @@ module "rds" {
 ##########################################
 
 module "cloudwatch" {
-  source = "./Module/CloudWatch"
-  vpc_name = var.project_name
+  source           = "./Module/CloudWatch"
+  vpc_name         = var.project_name
   ecs_cluster_name = module.ecs.cluster_name
   ecs_service_name = module.ecs.service_name
-  alb_arn_suffix = module.alb.alb_arn_suffix
-  rds_identifier = module.rds.db_identifier
+  alb_arn_suffix   = module.alb.alb_arn_suffix
+  rds_identifier   = module.rds.db_identifier
 
 }
--- Terrafrom/terrafrom.tfvars
+++ Terrafrom/terrafrom.tfvars
@@ -1,7 +1,7 @@
-aws_region = "ap-south-1"
-environment = "dev"
+aws_region   = "ap-south-1"
+environment  = "dev"
 project_name = "8byte"
-vpc_cidr = "10.0.0.0/16"
+vpc_cidr     = "10.0.0.0/16"
 public_subnet_cidrs = [
   "10.0.1.0/24",
   "10.0.2.0/24"
@@ -14,6 +14,6 @@ azs = [
   "ap-south-1a",
   "ap-south-1b"
 ]
-db_name = "appdb"
+db_name     = "appdb"
 db_username = "postgres"
 db_password = "Password123!"
