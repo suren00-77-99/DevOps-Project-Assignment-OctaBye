8f9f6605b91c8993712abf0c27582b202b85b693
Devops Suren <rajsuren1210@gmail.com>
2026-06-16 15:15:19 +0530

    Changed the db

--- Terraform/Module/RDS/main.tf
+++ Terraform/Module/RDS/main.tf
@@ -1,38 +1,29 @@
 resource "aws_db_subnet_group" "this" {
-  name       = "rds-${vpc_name}-subnet-group"
+  name = "rds-${var.vpc_name}-db-subnet-group"
   subnet_ids = var.private_subnet_ids
+  tags = merge(var.tags,{Name = "rds-${var.vpc_name}-db-subnet-group"})
 
-  tags = {
-    Name = "rds-${vpc_name}-subnet-group"
-  }
 }
 
+#DB instance
 resource "aws_db_instance" "this" {
-  identifier             = "rds-${vpc_name}-postgres"
-
-  engine                 = "postgres"
-  engine_version         = "16"
-  instance_class         = "db.t3.micro"
-
-  allocated_storage      = 20
-  storage_type           = "gp2"
-
-  db_name                = "appdb"
-  username               = "postgres"
-  password               = var.password
-
-  db_subnet_group_name   = aws_db_subnet_group.this.name
-
-  vpc_security_group_ids = [var.rds_sg_id]
-
-  publicly_accessible    = false
-  multi_az               = false
-
-  skip_final_snapshot    = true
-
-  backup_retention_period = 0
-
-  tags = {
-    Name = "rds-${vpc_name}"
-  }
+  identifier =  "rds-${var.vpc_name}-postgres"
+  engine = "postgres"
+  engine_version = "16.3"
+  instance_class = "db.t4.micro"
+  allocated_storage = 20
+  storage_type = "gp3"
+  db_name = var.db_name
+  username = var.username
+  password = var.password
+  skip_final_snapshot = true
+  publicly_accessible = false
+  multi_az = false
+  db_subnet_group_name = aws_db_subnet_group.this.name
+  vpc_security_group_ids = [
+    var.rds_sg_id
+  ]
+  backup_retention_period = 7
+  deletion_protection = false
+  tags = merge(var.tags,{Name = "rds-${var.vpc_name}-postgres"})
 }
