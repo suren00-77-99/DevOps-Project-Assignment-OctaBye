92d17182819752c948a126adb61c3252a2ee0404
Devops Suren <rajsuren1210@gmail.com>
2026-06-17 10:51:59 +0530

    checking the automation

--- Application-area/app.py
+++ Application-area/app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def home():
     return "8Byte DevOps Assignment Running Successfully"
 
-@app.route("/health")
-def health():
-    return {"status":"healthy"}
+# @app.route("/health")
+# def health():
+#     return {"status":"healthy"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
