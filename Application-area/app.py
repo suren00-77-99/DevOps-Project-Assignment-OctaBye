243027e109d7c44b414caece57cec40b37aa3694
Devops Suren <rajsuren1210@gmail.com>
2026-06-17 10:58:10 +0530

    added some text in py file

--- Application-area/app.py
+++ Application-area/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "8Byte DevOps Assignment Running Successfully"
+    return "8Byte DevOps Assignment Running Successfully\n Please check /health endpoint for health status"
 
-# @app.route("/health")
-# def health():
-#     return {"status":"healthy"}
+@app.route("/health")
+def health():
+    return {"status":"healthy"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
