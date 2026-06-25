41e6e7fee2c2ac3d165f41053b0ec0f3613104ff
Suren-devops <prithivisuren77@gmail.com>
2026-06-25 12:21:05 +0530

    eddited the py file.

--- Application-area/app.py
+++ Application-area/app.py
@@ -9,9 +9,9 @@ def home():
 what is your name? and what is your favorite place to visit?
 """
 
-@app.route("/health")
-def health():
-    return {"status":"healthy"}
+# @app.route("/health")
+# def health():
+#     return {"status":"healthy"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
